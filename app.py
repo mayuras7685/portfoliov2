6159028e8ae1a606c2b3fa13ffe5babc2b013630
mayuras7685 <mayurasodara@gmail.com>
2022-09-15 21:30:31 +0530

    v2.0

--- app.py
+++ app.py
@@ -4,8 +4,25 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-  return render_template('index.html')
+  return render_template('home.html')
+
+@app.route("/about")
+def about():
+  return render_template('home.html')
+
+@app.route("/projects")
+def projects():
+  return render_template('home.html')
+
+@app.route("/work")
+def work():
+  return render_template('home.html')
+
+@app.route("/certi")
+def certi():
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
