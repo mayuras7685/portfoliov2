b5747aa6e02fd2865125c2a548aaeca83d77fbff
mayuras7685 <mayurasodara@gmail.com>
2022-09-17 22:47:43 +0530

    contact page route

--- app.py
+++ app.py
@@ -6,24 +6,28 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-  return render_template('home.html')
+  return render_template('index.html')
 
 @app.route("/about")
 def about():
-  return render_template('home.html')
+  return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+  return render_template('contact.html')
 
 @app.route("/projects")
 def projects():
-  return render_template('home.html')
+  return render_template('projects.html')
 
 @app.route("/work")
 def work():
-  return render_template('home.html')
+  return render_template('work.html')
 
 @app.route("/certi")
 def certi():
-  return render_template('home.html')
+  return render_template('certificates.html')
 
 
 if __name__ == "__main__":
-  app.run(debug=False ,port=3000)
+  app.run(debug=True ,port=3000)
